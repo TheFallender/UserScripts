129aca630de92e5ef6d6764234e32f09fd5492ea
TheFallender <af.git@noxbit.net>
2023-10-08 10:32:24 +0200

    YT Shorts History - Fixed not working state

--- YoutubeShortsRemoveWH/YoutubeShortsRemoveWH.user.js
+++ YoutubeShortsRemoveWH/YoutubeShortsRemoveWH.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         Youtube Shorts - Remove from WH
 // @author       TheFallender
-// @version      1.1
+// @version      1.2.0
 // @description  This script will remove all the Shorts watched in your watch history.
 // @homepageURL  https://github.com/TheFallender/TamperMonkeyScripts
 // @updateURL    https://raw.githubusercontent.com/TheFallender/TamperMonkeyScripts/master/YoutubeShortsRemoveWH/YoutubeShortsRemoveWH.user.js
 // @downloadURL  https://raw.githubusercontent.com/TheFallender/TamperMonkeyScripts/master/YoutubeShortsRemoveWH/YoutubeShortsRemoveWH.user.js
-// @match        https://www.youtube.com/feed/history
+// @match        https://*.youtube.com/feed/history
 // @icon         https://www.google.com/s2/favicons?domain=youtube.com
 // @license      MIT
 // @copyright    Copyright © 2023 TheFallender
@@ -19,12 +19,13 @@
     'use strict';
 
     // Wait time: Time to wait between changes.
-    const timeBetweenWaits = 1000;
+    const timeBetweenWaits = 500;
 
     // Iterations limit
     const iterationsLimit = -1;
 
     // Selector items
+    const waitUntilReadySelector = "button#button.yt-icon-button[aria-label='Notifications']";
     const selShortsSelf = "ytd-reel-shelf-renderer";
     const selShortsselfItem = "div#menu.ytd-reel-item-renderer > ytd-menu-renderer > yt-icon-button > button";
     const selShorts = "ytd-video-renderer:has(ytd-thumbnail-overlay-time-status-renderer[overlay-style='SHORTS'])"
@@ -63,19 +64,18 @@
 
     // Wait until the shorts are loaded, if there are none
     // the script will be stalled here
-    waitForElement(selShorts).then(async (element) => {
+    waitForElement(waitUntilReadySelector).then(async (element) => {
         // Iteration limit
         for (let iterations = 0; iterations != iterationsLimit; iterations++, await sleep(3000)) {
+            console.log(`Iteration: ${iterations}`);
 
             // Get the first short shelf
             let shortsSelf = document.querySelector(selShortsSelf);
+            console.log(`ShortsSelf: ${shortsSelf ? shortsSelf.querySelectorAll(selShorts).length : 'Not found'}`);
 
             // Shorts
             let shorts = document.querySelectorAll(selShorts);
-
-            console.log(`Iteration: ${iterations}`);
             console.log(`Shorts: ${shorts.length}`);
-            console.log(`ShortsSelf: ${shortsSelf ? shortsSelf.querySelectorAll(selShorts).length : 'Not found'}`);
 
             // Scroll if not found
             if (shortsSelf == null && shorts.length == 0) {
@@ -85,7 +85,9 @@
             }
 
             // Clean the shorts list
-            shorts.forEach((short) => async () =>{
+            for (let i = 0; i < shorts.length; i++) {
+                let short = shorts[i];
+
                 // Click the button to delete it from the watch history
                 short.querySelector(selShortsButton)?.click();
 
@@ -94,11 +96,17 @@
 
                 // Wait the time for the next short
                 await sleep(timeBetweenWaits);
-            });
+            }
 
             // Get the shorts list
-            let shortsList = shortsSelf.querySelectorAll(selShorts);
-            shortsList.forEach((short) => async () =>{
+            if (shortsSelf == null) {
+                continue;
+            }
+
+            let shortsList = shortsSelf?.querySelectorAll(selShorts);
+            for (let i = 0; i < shortsList.length; i++) {
+                let short = shortsList[i];
+
                 // Click the button to show the toast
                 short.click();
                 await sleep(250);
@@ -118,7 +126,7 @@
 
                 // Wait the time for the next short
                 await sleep(timeBetweenWaits);
-            });
+            }
 
             // Remove the list
             shortsSelf.remove()
