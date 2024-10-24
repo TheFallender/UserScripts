362e5f45d4705686e37e57a26ac416b46d58ac35
TheFallender <af.git@noxbit.net>
2024-10-24 12:02:08 +0200

    SteamShortShare - Valve removed the share, I added it back

--- SteamShortShare/SteamShortShare.user.js
+++ SteamShortShare/SteamShortShare.user.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Steam Short Share
 // @author       TheFallender
-// @version      1.0.1
+// @version      1.0.2
 // @description  Will replace the links of sharing with the s.team/a/<id> format
 // @homepageURL  https://github.com/TheFallender/TamperMonkeyScripts
 // @updateURL    https://raw.githubusercontent.com/TheFallender/TamperMonkeyScripts/master/SteamShortShare/SteamShortShare.user.js
 // @downloadURL  https://raw.githubusercontent.com/TheFallender/TamperMonkeyScripts/master/SteamShortShare/SteamShortShare.user.js
 // @supportURL   https://github.com/TheFallender/TamperMonkeyScripts
-// @match        https://store.steampowered.com/app/*
+// @match        https://store.steampowered.com/*
 // @icon         https://www.google.com/s2/favicons?domain=s.team
 // @license      MIT
 // @copyright    Copyright © 2024 TheFallender
@@ -18,7 +18,7 @@
     'use strict';
 
     // Selectors
-    const shareButtonSel = 'a[onclick*="ShowShareDialog"]';
+    const shareButtonSel = '#queueActionsCtn > div';
     const gameId = document.location.pathname.match(/\/app\/(\d+)\//)[1];
 
     //Method to wait for an element in the DOM
@@ -64,6 +64,29 @@
 
     //Replace share links
     waitForElement(shareButtonSel, false).then((element) => {
-        element.onclick = (event) => { navigator.clipboard.writeText(`https://s.team/a/${gameId}`); };
+        // Outer Div
+        const queueBtnShare = document.createElement('div');
+        queueBtnShare.id = 'queueBtnShare';
+        queueBtnShare.classList.add('queue_control_button');
+        queueBtnShare.style.flexGrow = '0';
+
+        // Button Div
+        const innerDiv = document.createElement('div');
+        innerDiv.classList.add('btnv6_blue_hoverfade', 'btn_medium', 'queue_btn_inactive');
+        innerDiv.setAttribute('data-panel', '{"focusable":true,"clickOnActivate":true}');
+        innerDiv.setAttribute('data-tooltip-text', 'Quickly share your Steam links.');
+
+        // Span with Share
+        const spanElement = document.createElement('span');
+        spanElement.appendChild(document.createTextNode(' Share'));
+
+        // Closing Divs
+        innerDiv.appendChild(spanElement);
+        queueBtnShare.appendChild(innerDiv);
+
+        const targetDiv = document.querySelector('#queueActionsCtn > div[style="clear: both;"]');
+
+        let finalShareButton = targetDiv.parentNode.insertBefore(queueBtnShare, targetDiv);
+        finalShareButton.onclick = (event) => { navigator.clipboard.writeText(`https://s.team/a/${gameId}`); };
     });
 })();
